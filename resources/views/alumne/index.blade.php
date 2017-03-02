e334f92c3ccee445a09a5a2053d559c2641f3238
Gitkikaso <grupo4s2j@gmial.com>
2017-03-02 20:50:45 +0100

    form alumno

--- resources/views/alumne/index.blade.php
+++ resources/views/alumne/index.blade.php
@@ -329,6 +329,121 @@
                 </div>
 
             </div>
+            <div class="col-md-6">
+                <div class="box box-primary">
+                    <div class="box-header with-border">
+                        <h3 class="box-title">Carnet</h3>
+                    </div>
+                    <!-- /.box-header -->
+                    <!-- form start -->
+                    <form role="form">
+                        <div class="box-body">
+                            <form action="/" method="post">
+                                {!! csrf_field() !!}
+                                <input type="hidden" name="user_id" value="1">
+                                <div class="form-group">
+                                    <select name="role_name" id="" class="form-control">
+
+                                        <option value="1">aaaa</option>
+
+                                    </select>
+                                </div>
+                                <div class="form-group">
+                                    <button class='btn btn-primary'>Add carnet</button>
+                                </div>
+                            </form>
+                            <table class='table'>
+                                <thead>
+                                <th>Codi</th>
+                                <th>Action</th>
+                                </thead>
+                                <tbody>
+
+                                <tr>
+                                    <td>aaaa</td>
+                                    <td><a href="/" class="btn btn-danger btn-sm"><i class="fa fa-trash-o"
+                                                                                     aria-hidden="true"></i></a></td>
+                                </tr>
+
+                                </tbody>
+                            </table>
+
+                        </div>
+                        <!-- /.box-body -->
+
+                        <div class="box-footer">
+                            {{--<button type="submit" class="btn btn-primary">Submit</button>--}}
+                        </div>
+                    </form>
+                </div>
+
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-md-12">
+                <div class="box box-primary">
+                    <div class="box-header with-border">
+                        <h3 class="box-title">Altres dades d'interess</h3>
+                    </div>
+                    <!-- /.box-header -->
+                    <!-- form start -->
+                    <form role="form">
+                        <div class="box-body">
+                            <div class="col-xs-6">
+                                <div class="form-group">
+                                    <label for="exampleInputEmail1">comentari</label>
+                                    <input type="text" class="form-control" id="exampleInputEmail1"
+                                           placeholder="placeholder text">
+                                </div>
+                            </div>
+                            <div class="col-xs-6">
+                                <div class="form-group">
+                                    <label for="exampleInputEmail1">Estudia ara</label>
+                                    <input type="text" class="form-control" id="exampleInputEmail1"
+                                           placeholder="placeholder text">
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <div class="col-xs-6">
+                                    <label for="exampleInputEmail1">mobilitat</label>
+                                    <input type="text" class="form-control" id="exampleInputEmail1"
+                                           placeholder="placeholder text">
+                                </div>
+                                <div class="col-xs-6">
+                                    <label for="exampleInputEmail1">Radi mobilitat</label>
+                                    <input type="text" class="form-control" id="exampleInputEmail1"
+                                           placeholder="placeholder text">
+                                </div>
+                            </div>
+                            <div class="col-xs-6">
+                                <div class="form-group">
+                                    <label for="exampleInputEmail1">discapacitat</label>
+                                    <input type="text" class="form-control" id="exampleInputEmail1"
+                                           placeholder="placeholder text">
+                                </div>
+                                <div class="form-group">
+                                    <label for="exampleInputEmail1">grau discapacitat</label>
+                                    <input type="text" class="form-control" id="exampleInputEmail1"
+                                           placeholder="placeholder text">
+                                </div>
+                            </div>
+                            <div class="col-xs-6">
+                                <div class="form-group">
+                                    <label for="exampleInputEmail1">altres</label>
+                                    <textarea type="text" class="form-control" id="exampleInputEmail1"
+                                              placeholder="placeholder text"></textarea>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- /.box-body -->
+
+                        <div class="box-footer">
+                            <button type="submit" class="btn btn-primary">Submit</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
         </div>
     </section>
 
