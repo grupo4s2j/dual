f0d4daf10fe215bcebd0081307f117e3f4950150
Gitkikaso <grupo4s2j@gmial.com>
2017-03-03 16:48:18 +0100

    new style forms

---
+++ resources/views/alumne/estudis.blade.php
@@ -0,0 +1,83 @@
+<h3>Estudis</h3>
+<div class="row">
+    <div class="col-md-6">
+        <div class="box box-primary">
+            <div class="box-header with-border">
+                <h3 class="box-title">Estudis Reglats</h3>
+            </div>
+            <!-- /.box-header -->
+            <!-- form start -->
+            <form role="form">
+                <div class="box-body">
+                    <div class="form-group">
+                        <label for="exampleInputEmail1">Desc Centre</label>
+                        <input type="text" class="form-control" id="exampleInputEmail1"
+                               placeholder="placeholder text">
+                    </div>
+                    <div class="form-group">
+                        <label for="exampleInputEmail1">Any Obtencio</label>
+                        <input type="text" class="form-control" id="exampleInputEmail1"
+                               placeholder="placeholder text">
+                    </div>
+                    <div class="form-group">
+                        <label for="exampleInputEmail1">Nota Expedient</label>
+                        <input type="text" class="form-control" id="exampleInputEmail1"
+                               placeholder="placeholder text">
+                    </div>
+                    <div class="form-group">
+                        <label for="exampleInputEmail1">Estudi ->>> DROPDOWN</label>
+                        <input type="text" class="form-control" id="exampleInputEmail1"
+                               placeholder="placeholder text">
+                    </div>
+                </div>
+                <!-- /.box-body -->
+
+                <div class="box-footer">
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </div>
+            </form>
+        </div>
+
+
+    </div>
+    <div class="col-md-6">
+        <div class="box box-primary">
+            <div class="box-header with-border">
+                <h3 class="box-title">Estudis NO Reglats</h3>
+            </div>
+            <!-- /.box-header -->
+            <!-- form start -->
+            <form role="form">
+                <div class="box-body">
+                    <div class="form-group">
+                        <label for="exampleInputEmail1">Desc Centre</label>
+                        <input type="text" class="form-control" id="exampleInputEmail1"
+                               placeholder="placeholder text">
+                    </div>
+                    <div class="form-group">
+                        <label for="exampleInputEmail1">Desc Estudi</label>
+                        <input type="text" class="form-control" id="exampleInputEmail1"
+                               placeholder="placeholder text">
+                    </div>
+                    <div class="form-group">
+                        <label for="exampleInputEmail1">Any Obtencio</label>
+                        <input type="text" class="form-control" id="exampleInputEmail1"
+                               placeholder="placeholder text">
+                    </div>
+                    <div class="form-group">
+                        <label for="exampleInputEmail1">Horas</label>
+                        <input type="text" class="form-control" id="exampleInputEmail1"
+                               placeholder="placeholder text">
+                    </div>
+
+                </div>
+                <!-- /.box-body -->
+
+                <div class="box-footer">
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </div>
+            </form>
+        </div>
+
+    </div>
+</div>
