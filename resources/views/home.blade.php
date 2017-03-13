19970a19e8d1dfb0e1cca212c26c57ee86e88967
GonzaloMF <gmandrion@referup.com>
2017-03-13 18:32:39 +0100

    login

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,12 +5,12 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
+                <a href="{{ url('/dashboard') }}">BACKOFFICE</a>
 
                 <div class="panel-body">
-                    You are logged in!
+                    <h1>Bienvenido {{auth::user()->name}} !!!!</h1>
+
                 </div>
-            </div>
         </div>
     </div>
 </div>
