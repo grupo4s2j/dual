06f2253a4df892ee6669bcb97f915c0f48fac311
Gitkikaso <grupo4s2j@gmial.com>
2017-03-14 20:40:38 +0100

    drop douwn area

--- resources/views/alumne/estudis.blade.php
+++ resources/views/alumne/estudis.blade.php
@@ -1,4 +1,3 @@
-
 <div class="row">
     <div class="col-md-6">
         <div class="box box-primary">
@@ -64,32 +63,39 @@
             </div>
             <!-- /.box-header -->
             <!-- form start -->
-            <form method='POST' action='{!! url("alumne")!!}/{!!$alumne->id!!}/updateEstudiNoReglat' enctype="multipart/form-data" class="form-horizontal">
+            <form method='POST' action='{!! url("alumne")!!}/{!!$alumne->id!!}/updateEstudiNoReglat'
+                  enctype="multipart/form-data" class="form-horizontal">
                 <input type='hidden' name='_token' value='{{Session::token()}}'>
-            <div class="box-body">
+                <div class="box-body">
                     <div class="form-group">
-                        <label for="exampleInputEmail1">Area</label>
-                        <input type="text" class="form-control" id="exampleInputEmail1"
+                        <label for="idArea">Area</label>
+
+                        <select  class="form-control"  id="idArea" name="idArea">
+                            @foreach($areas as $area)
+                                <option value={{$area->id}}>{{$area->codiArea}}</option>
+                            @endforeach
+                        </select>
+
+
+                    </div>
+                    <div class="form-group">
+                        <label for="descCentro">Desc Centre</label>
+                        <input type="text" class="form-control" id="descCentro" name="descCentro"
                                placeholder="placeholder text">
                     </div>
                     <div class="form-group">
-                        <label for="exampleInputEmail1">Desc Centre</label>
-                        <input type="text" class="form-control" id="exampleInputEmail1"
+                        <label for="descEstudio">Desc Estudi</label>
+                        <input type="text" class="form-control" id="descEstudio" name="descEstudio"
                                placeholder="placeholder text">
                     </div>
                     <div class="form-group">
-                        <label for="exampleInputEmail1">Desc Estudi</label>
-                        <input type="text" class="form-control" id="exampleInputEmail1"
+                        <label for="añoObtencion">Any Obtencio</label>
+                        <input type="text" class="form-control" id="añoObtencion" name="añoObtencion"
                                placeholder="placeholder text">
                     </div>
                     <div class="form-group">
-                        <label for="exampleInputEmail1">Any Obtencio</label>
-                        <input type="text" class="form-control" id="exampleInputEmail1"
-                               placeholder="placeholder text">
-                    </div>
-                    <div class="form-group">
-                        <label for="exampleInputEmail1">Horas</label>
-                        <input type="text" class="form-control" id="exampleInputEmail1"
+                        <label for="horas">Horas</label>
+                        <input type="text" class="form-control" id="horas" name="horas"
                                placeholder="placeholder text">
                     </div>
 
@@ -107,12 +113,13 @@
                 </thead>
                 <tbody>
                 @foreach($estudisnoreglats as $estudisnoreglat)
-                <tr>
+                    <tr>
 
-                    <td>{{$estudisnoreglat->id}}</td>
-                    <td><a href="alumne/{{$estudisnoreglat->id}}/deleteEstudiNoReglat" class="btn btn-danger btn-sm"><i class="fa fa-trash-o"aria-hidden="true"></i></a></td>
+                        <td>{{$estudisnoreglat->id}}</td>
+                        <td><a href="alumne/{{$estudisnoreglat->id}}/deleteEstudiNoReglat"
+                               class="btn btn-danger btn-sm"><i class="fa fa-trash-o" aria-hidden="true"></i></a></td>
 
-                </tr>
+                    </tr>
                 @endforeach
                 </tbody>
             </table>
