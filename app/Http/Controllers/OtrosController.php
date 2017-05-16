d6155c0f42f53e6940fc7068bc8028cbfcf17e39
GitCarlosMoreno <carlos.mogui95@gmail.com>
2017-05-16 19:47:11 +0200

    creates

--- app/Http/Controllers/OtrosController.php
+++ app/Http/Controllers/OtrosController.php
@@ -69,8 +69,8 @@ class OtrosController extends Controller
     {
         if (Auth::check() and Auth::user()->rol == 0) {
             $objects = poblacions::get();
-
-            return view('scaffold-interface.otros.poblacions', compact('objects'));
+            $provincies = provincies::get();
+            return view('scaffold-interface.otros.poblacions', compact('objects','provincies'));
         }
         return redirect('home');
     }
@@ -151,7 +151,7 @@ class OtrosController extends Controller
         if (Auth::check() and Auth::user()->rol == 0) {
             $obj = sectors::findOrfail($id);
             if ($obj->delete()) {
-                return redirect('/admin/otros/sectors');
+                return redirect('/admin/otros/sector');
             } else {
                 $error = '<div class="alert alert-danger">  <strong>Error</strong> Hay alumnos en este sector.</div>';
                 $objects = sectors::get();
@@ -225,5 +225,80 @@ class OtrosController extends Controller
 
             return redirect('admin/otros/skills');
     }
+    public function storeIdiomes(Request $request)
+    {
+        $obj = new idiomes();
+        $obj->codiIdioma = $request->codiIdioma;
+        $obj->descIdioma = $request->descIdioma;
 
+
+        $obj->save();
+
+
+        return redirect('admin/otros/idiomes');
+    }
+
+    public function storeEstudis(Request $request)
+    {
+        $obj = new estudis();
+        $obj->codiEstudio = $request->codiEstudio;
+        $obj->descEstudio = $request->descEstudio;
+        $obj->numHoras  = $request-> numHoras;
+
+
+        $obj->save();
+
+
+        return redirect('admin/otros/estudis');
+    }
+
+    public function storeSector(Request $request)
+    {
+        $obj = new sectors();
+        $obj->codiSector = $request->codiSector;
+        $obj->descSector = $request->descSector;
+
+
+        $obj->save();
+
+
+        return redirect('admin/otros/sector');
+    }
+
+    public function storeFamiliesprofesionals(Request $request)
+    {
+        $obj = new families();
+        $obj->codiFamilia = $request->codiFamilia;
+        $obj->descFamilia = $request->descFamilia;
+
+
+        $obj->save();
+
+
+        return redirect('admin/otros/familiesprofesionals');
+    }
+    public function storePoblacions(Request $request)
+    {
+        $obj = new poblacions();
+        $obj->poblacio = $request->poblacio;
+
+
+
+        $obj->save();
+
+
+        return redirect('admin/otros/poblacions');
+    }
+    public function storeProvincies(Request $request)
+    {
+        $obj = new provincies();
+        $obj->provincia = $request->provincia;
+
+
+
+        $obj->save();
+
+
+        return redirect('admin/otros/provincies');
+    }
 }
--- resources/views/scaffold-interface/otros/poblacions.blade.php
+++ resources/views/scaffold-interface/otros/poblacions.blade.php
@@ -16,9 +16,8 @@
         }; ?>
 
         <div class="tab-pane active" id="tab_1">
-            <form class='col s3' method='get' action='{!!url("alumne")!!}/create'>
-                <button class='btn btn-primary' type='submit'>Crear poblacion</button>
-            </form>
+            <button type="button" class='btn btn-primary' data-toggle="modal" data-target="#myModal">Crear poblacion
+            </button>
             <br>
 
             <table id="example1" class="table table-striped table-bordered table-hover"
@@ -67,7 +66,45 @@
         <!-- /.tab-content -->
         </div>
 
+        <!-- Modal -->
+        <div id="myModal" class="modal fade" role="dialog">
+            <form class='col s3' method='get' action='{{url('admin/otros/poblacions')}}/create'>
+                <div class="modal-dialog">
 
+                    <!-- Modal content-->
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <button type="button" class="close" data-dismiss="modal">&times;</button>
+                            <h4 class="modal-title">Añadir poblacio</h4>
+                        </div>
+                        <div class="modal-body">
+                            <div class="form-group">
+                                <label for="poblacio">Poblacio</label>
+                                <input id="poblacio" name="poblacio" type="text" class="form-control"  required>
+                            </div>
+                        </div>
+                        <div class="modal-body">
+                            <div class="form-group">
+                                <label for="poblacio">Poblacio</label>
+                                <select name="idProvincia" id="idProvincia" class="form-control">
+                                    @foreach($provincies as $ss)
+                                        <option value="{{$ss->id}}">{{$ss->provincia}}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+                        </div>
+
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+
+                            <button class='btn btn-primary pull-left' type='submit'>Crear poblacion</button>
+
+                        </div>
+                    </div>
+
+                </div>
+            </form>
+        </div>
 
     </section>
 @endsection
