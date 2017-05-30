a0922d141e4700b12dbf580eb4a0559740298c87
BryanGonzalez <bryangoyo@outllok.com>
2017-05-30 17:34:08 +0200

    Deleted Directorio Empresa

--- resources/views/empresa/registroEmpresa/tabsRegistro.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('scaffold-interface.layouts.appMenuHorizontal')
-@section('topNavigation', 'Empresa')
-@section('menu_options')
-
-<li class="active"><a data-toggle="tab" href="#menu1">Datos empresa</a></li>
-<li><a data-toggle="tab" href="#menu2">Persona de contacto</a></li>
-
-@endsection
-
